4d13c25099cfd11adde5bbd78233afeb1316bafa
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-08-08 14:22:29 -0500

    Minor tweaks
    - Removed python-shell from dependencies (no longer using it)
    - To do comment for menu

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
-const electron = require('electron')
-const url = require('url')
-const path = require('path')
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
 
-const { app, BrowserWindow, Menu, ipcMain} = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 
-app.on('ready', function () {
-    mainWindow = new BrowserWindow({
-        width: 600,
-        height: 630,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true
-        }
-    });
-    mainWindow.resizable = false;
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'src/mainWindow.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show()
-    });
-
-    ipcMain.on('close:app', (evt, arg) => {
-        app.quit()
+app.on("ready", function () {
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 630,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+  mainWindow.resizable = false;
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "src/mainWindow.html"),
+      protocol: "file:",
+      slashes: true,
     })
+  );
 
-    // const mainMenu = Menu.buildFromTemplate(mainMenuTemp);
-    // TO DO: Possibly move quit and start functionality 
-    // to Menu
-    //Menu.setApplicationMenu(mainMenu)
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
 
+  ipcMain.on("close:app", (evt, arg) => {
+    app.quit();
+  });
+
+  // const mainMenu = Menu.buildFromTemplate(mainMenuTemp);
+  // TO DO: Possibly move quit and start functionality
+  // to Menu
+  //Menu.setApplicationMenu(mainMenu)
 });
 
-
+// To Do: Move Quit and Settings to Menu
 /* const mainMenuTemp = [
     {
         label: 'File',
